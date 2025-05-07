05caf4650caed89c9f55709cdb782f3109cbc41d
Xi Chen <xinehc@gmail.com>
2025-05-07 17:25:55 +0800

    rich argparse

--- src/argo/cli.py
+++ src/argo/cli.py
@@ -1,28 +1,37 @@
 import sys
 import os
 import glob
+import argparse
 
-from argparse import ArgumentParser, SUPPRESS
+from rich_argparse import ArgumentDefaultsRichHelpFormatter
 from melon.utils import logger, get_filename
 from melon import GenomeProfiler
 from . import __version__, AntibioticResistanceGeneProfiler
+
+## customize formatter
+ArgumentDefaultsRichHelpFormatter.styles['argparse.prog'] = 'default'
+ArgumentDefaultsRichHelpFormatter.styles['argparse.default'] = 'grey50'
+ArgumentDefaultsRichHelpFormatter.styles['argparse.metavar'] = 'grey50'
+ArgumentDefaultsRichHelpFormatter.styles['argparse.groups'] = '#E34234'
+ArgumentDefaultsRichHelpFormatter.styles['argparse.args'] = 'default'
 
 
 def cli(argv=sys.argv):
     '''
     Entry point for command line interface.
     '''
-    parser = ArgumentParser(description='Argo: species-resolved profiling of antibiotic resistance genes in complex metagenomes through long-read overlapping', add_help=False)
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
-    additional = parser.add_argument_group('additional arguments')
-    additional_mcl = parser.add_argument_group('additional arguments for MCL')
+    parser = argparse.ArgumentParser(
+        description=f'Argo v{__version__}: species-resolved profiling of antibiotic resistance genes in complex metagenomes through long-read overlapping',
+        formatter_class=ArgumentDefaultsRichHelpFormatter,
+        add_help=False)
 
     parser.add_argument(
-        'FILE',
+        dest='files',
         nargs='+',
+        metavar='file',
         help='Input fasta <*.fa|*.fasta> or fastq <*.fq|*.fastq> file, gzip optional <*.gz>.')
 
+    required = parser.add_argument_group('required arguments')
     required.add_argument(
         '-d',
         '--db',
@@ -37,6 +46,7 @@ def cli(argv=sys.argv):
         required=True,
         help='Output folder.')
 
+    optional = parser.add_argument_group('optional arguments')
     optional.add_argument(
         '-t',
         '--threads',
@@ -60,85 +70,87 @@ def cli(argv=sys.argv):
         action='store_true',
         help='Skip cleaning, keep all temporary <*.tmp> files.')
 
+    additional = parser.add_argument_group('additional arguments - filtering')
     additional.add_argument(
         '-m',
         metavar='INT',
         type=int,
         default=25,
-        help='Max. number of target sequences to report (--max-target-seqs/-k in diamond). [25]')
+        help='Max. number of target sequences to report (--max-target-seqs/-k in diamond).')
 
     additional.add_argument(
         '-e',
         metavar='FLOAT',
         type=float,
         default=1e-5,
-        help='Max. expected value to report alignments (--evalue/-e in diamond). [1e-5]')
+        help='Max. expected value to report alignments (--evalue/-e in diamond).')
 
     additional.add_argument(
         '-i',
         metavar='FLOAT',
         type=float,
         default=0,
-        help='Min. identity in percentage to report alignments. If "0" then set 90 - 2.5 * 100 * median sequence divergence. [0]')
+        help='Min. identity in percentage to report alignments. If "0" then set 90 - 2.5 * 100 * median sequence divergence.')
 
     additional.add_argument(
         '-s',
         metavar='FLOAT',
         type=float,
         default=90,
-        help='Min. subject cover within a read cluster to report alignments. [90]')
+        help='Min. subject cover within a read cluster to report alignments.')
 
     additional.add_argument(
         '-n',
         metavar='INT',
         type=int,
         default=2147483647,
-        help='Max. number of secondary alignments to report (-N in minimap2). [2147483647]')
+        help='Max. number of secondary alignments to report (-N in minimap2).')
 
     additional.add_argument(
         '-p',
         metavar='FLOAT',
         type=float,
         default=0.9,
-        help='Min. secondary-to-primary score ratio to report secondary alignments (-p in minimap2). [0.9]')
+        help='Min. secondary-to-primary score ratio to report secondary alignments (-p in minimap2).')
 
     additional.add_argument(
         '-z',
         metavar='FLOAT',
         type=float,
         default=1,
-        help='Min. estimated genome copies of a species to report it ARG copies and abundances. [1]')
+        help='Min. estimated genome copies of a species to report it ARG copies and abundances.')
 
     additional.add_argument(
         '-u',
         metavar='INT',
         type=int,
         default=0,
-        help='Max. number of ARG-containing reads per chunk for overlapping. If "0" then use a single chunk. [0]')
+        help='Max. number of ARG-containing reads per chunk for overlapping. If "0" then use a single chunk.')
 
-    additional_mcl.add_argument(
+    additional = parser.add_argument_group('additional arguments - graph clustering')
+    additional.add_argument(
         '-b',
         metavar='INT',
         type=int,
         default=1000,
-        help='Terminal condition - max. iterations. [1000]')
+        help='Terminal condition - max. iterations.')
 
-    additional_mcl.add_argument(
+    additional.add_argument(
         '-x',
         metavar='FLOAT',
         type=float,
         default=2,
-        help='Graph clustering parameter - inflation. [2]')
+        help='MCL parameter - inflation.')
 
-    additional_mcl.add_argument(
+    additional.add_argument(
         '-y',
         metavar='FLOAT',
         type=float,
         default=2,
-        help='Graph clustering parameter - expansion. [2]')
+        help='MCL parameter - expansion.')
 
-    parser.add_argument('-v', '--version', action='version', version=__version__, help=SUPPRESS)
-    parser.add_argument('-h', '--help', action='help', help=SUPPRESS)
+    parser.add_argument('-v', '--version', action='version', version=__version__, help=argparse.SUPPRESS)
+    parser.add_argument('-h', '--help', action='help', help=argparse.SUPPRESS)
 
     if len(argv) == 1:
         print(f"\
@@ -163,7 +175,7 @@ def run(opt):
         logger.warning(f'Folder <{opt.output}> exists. Files will be overwritten.')
 
     ## check for input files
-    for file in opt.FILE:
+    for file in opt.files:
         if not os.path.isfile(file):
             logger.critical(f'File <{file}> does not exist.')
             sys.exit(2)
@@ -190,9 +202,9 @@ def run(opt):
         opt.threads = os.cpu_count()
 
     ## run
-    for index, file in enumerate(opt.FILE):
-        if len(opt.FILE) > 1:
-            logger.info(f'Processing file <{file}> ({index + 1}/{len(opt.FILE)}) ...')
+    for index, file in enumerate(opt.files):
+        if len(opt.files) > 1:
+            logger.info(f'Processing file <{file}> ({index + 1}/{len(opt.files)}) ...')
 
         if not opt.skip_melon or any(not os.path.isfile(get_filename(file, opt.output, ext)) for ext in ['.tsv', '.json']):
             GenomeProfiler(file, opt.db, opt.output, opt.threads).run(skip_clean=opt.skip_clean)
@@ -206,7 +218,7 @@ def run(opt):
             min_genome_copies=opt.z, chunk_size=opt.u,
             max_iterations=opt.b, inflation=opt.x, expansion=opt.y)
 
-        if index == len(opt.FILE) - 1:
+        if index == len(opt.files) - 1:
             logger.info('Done.')
         else:
             logger.info('Done.\n')
